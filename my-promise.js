149c0da6d3f34b83c5875decdc2376492f1a60f5
Gagandeep <gagan.ddun@gmail.com>
2023-01-31 22:00:07 +1100

    handle success /failure

--- my-promise.js
+++ my-promise.js
@@ -1,19 +1,79 @@
+const STATE = {
+    PENDING: 'pending',
+    FULFILLED: 'fulfilled',
+    REJECTED: 'rejected'
+}
+
 class MyPromise {
+    #thenCallbacks = []
+    #catchCallbacks = []
+    #state = STATE.PENDING
+    #value
+
     constructor(callback) {
         try {
-        callback(this.onSuccess, this.onFail)
+        callback(this.#onSuccess, this.#onFail)
         } catch(err) {
-            this.onFail(err)
+            console.log('catch')
+            this.#onFail(err)
         }
     }
 
-    onSuccess() {
-        
+    #runCallbacks() {
+        if (this.#state === STATE.FULFILLED) {
+            this.#thenCallbacks.forEach(callback => {
+                callback(this.#value)
+            })
+
+            this.#thenCallbacks = []
+        }
+
+        if (this.#state === STATE.REJECTED) {
+            this.#catchCallbacks.forEach(callback => {
+                callback(this.#value)
+            })
+
+            this.#catchCallbacks = []
+        }
     }
 
-    onFail() {
-        
+    #onSuccess(value) {
+        console.log('onSuccess ', this.#state)
+        if (this.#state != STATE.PENDING) return;
+
+        this.#state = STATE.FULFILLED;
+        this.#value = value
+
+        this.#runCallbacks()
+    }
+
+    #onFail(value) {
+        console.log('onFail ', this.#state)
+        if (this.#state != STATE.PENDING) return;
+
+        this.#state = STATE.REJECTED
+        this.#value = value
+
+        this.#runCallbacks()
+    }
+
+    then(callback) {
+        this.#thenCallbacks.push(callback)
+
+        this.#runCallbacks()
     }
 }
 
 module.exports = MyPromise;
+
+
+const p = new MyPromise((resolve, reject) => {
+    resolve(500)
+});
+p.then((res) => {
+    console.log(res)
+})
+
+p.then(res => {
+    console.log('> ', res)
+})
